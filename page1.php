40b21983ca2b827c92e8ece315a0034d2c7ec528
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-01-31 16:33:59 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,8 +1,22 @@
 <html>
   <head>
-    <title>งานที่ 1</title>
+    <title>page 1</title>
   </head>
-  <body>
-    page1
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br>
+    <h2>
+      <b>ชื่อ :</b> <i>นางสาวชุณหกาญจน์  ศรีวัฒนกุล</i>
+    <br>
+      <b>ชื่อเล่น :</b> <i>หยก</i>
+    <br>
+      <b>ชั้น :</b> <i>ม.4/12</i>
+    <br>
+      <b>เลขที่ :</b> <i>14</i>
+    <br>
+      <b>เลขประจำตัวนักเรียน :</b> <i>57052</i>
+    <br>
+      <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i>
+    </h2>  
   </body>
 </html>
