4584119b62ba9a39b021c36fb944c32b4ef69037
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-02-11 20:59:13 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -4,6 +4,6 @@
   </head>
   <body>
     <a href="https://facebook.com"><h1>Facebook</h1></a>
-    <a href="mypage2.php"><h1>Mypage2</h1></a>
+    <a href="mypage2.php"><h1>mypage2</h1></a>
   </body>
 </html>
