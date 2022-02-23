24c9d26e1df2882b434820115de47db74c63b405
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-02-23 15:03:55 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+   <tittle>การกำหนดรูปแบบตัวอักษร</tittle>
+   <style>
+     @font-face{
+       font-family: "MyFont1";
+       src: url("ChocalateteehaypaiTEST.ttf");
+     }
+     @font-face{
+      font-family: "MyFont2";
+      src: url("MNNTuaklom.ttf");
+     }
+     #font1{
+       font-family: "MyFont1";
+     }
+     #font2{
+      font-family: "MyFont2";
+     }
+   </style>
+  </head>
+  <body>
+     <h1 id="font1">นางสาวชุณหกาญจน์ ศรีวัฒยกุล เลขที่ 14 ห้อง ม.4/12 เลขประจำตัว 57052</h1>
+     <h1 id="font2">นางสาวชุณหกาญจน์ ศรีวัฒยกุล เลขที่ 14 ห้อง ม.4/12 เลขประจำตัว 57052</h1>
+  </body>
+</html>
