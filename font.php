822260ab8800c4c9619df1f23877d5a4ba4383a7
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-02-23 13:19:52 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("MNNTuaklom.ttf");
+      }
+      body{
+        font-family: "MyFont1";
+        src: url("ChocolateteehaypaiTEST.ttf");
+      }
+    </style>
+  </head>
+  <body>
+    <h1>ฟอนต์ของฉัน</h1>
+  </body>
+</html>
