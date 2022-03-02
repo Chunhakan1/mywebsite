5d49daf70c712083ffe4431e9a4e5448242ebe3d
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-03-02 17:16:01 +0700

    Update table.php

--- table.php
+++ table.php
@@ -14,14 +14,33 @@
     <tr>
       <td>1</td>
       <td>โบว์ ศิรดา</td>
-      <td><img scr=""45px"></td>
+      <td><img scr="" width="45px"></td>
       <td><a href=https://www.facebook.com/sirada.sue">Facebook</a></td>
     </tr>  
     <tr>
       <td>2</td>
       <td>ปั๋น ดริสา</td>
-      <td><img scr="https://sgp1.digitaloceanspaces.com/adaybulletin/2021/04/Open-WEB-The-Guest-riety.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/rietypahn">Facebook</a></td>  
+      <td><img scr="" width="45px"></td>
+      <td><a href="https://www.facebook.com/rietypahn">Facebook</a></td>  
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>ปอร์เช่ ศิวกร</td>
+      <td><img scr="" width="45px"</td>
+      <td><a href="https://www.instagram.com/porsche.sivk/?hl=en">Instagram</a></td>  
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>แจ๊คกี้ จักริน</td>
+        <td><img scr="" width="45px"></td>
+        <td><a href="https://www.instagram.com/j.jackr/?hl=en">Instagram</a></td>
+    </tr> 
+    <tr>
+        <td>5</td>
+        <td>เติร์ด ลภัส</td>
+        <td><img scr="" width="45px"></td>
+        <td><a href="https://www.instagram.com/thirdd/>Instagram</a></td>
+    </tr>
    </table>
   </body>
 </html>
