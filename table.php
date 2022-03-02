ff19c02d6d675dd8b0d4fb65c0dd0a15ed1137c3
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-03-02 15:29:08 +0700

    Update table.php

--- table.php
+++ table.php
@@ -14,7 +14,7 @@
     <tr>
       <td>1</td>
       <td>โบว์ ศิรดา</td>
-      <td><img scr="https://www.scbfoundation.com/stocks/images/87/215x215/15329621250iutr87.jpg" width="45px"></td>
+      <td><img scr="นางสาวศิรดา สื่อไพศาล_copy.jpg" width="45px"></td>
       <td><a href="https://www.facebook.com/search/top?q=siradasue">Facebook</a></td>
     </tr>  
    </table>
