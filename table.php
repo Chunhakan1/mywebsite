be8cdeb60f04ea6a05fbf3285cfd37e5262b9a40
Chunhakan1 <97584457+Chunhakan1@users.noreply.github.com>
2022-03-02 17:43:35 +0700

    Update table.php

--- table.php
+++ table.php
@@ -19,25 +19,25 @@
     <tr>
       <td>2</td>
       <td>ปั๋น ดริสา</td>
-      <td><img scr="" width="45px"></td>
+      <td><img scr="https://sgp1.digitaloceanspaces.com/adaybulletin/2021/04/Open-WEB-The-Guest-riety.jpg" width="45px"></td>
       <td><a href="https://www.facebook.com/rietypahn">Facebook</a></td>  
     </tr>
     <tr>
       <td>3</td>
       <td>ปอร์เช่ ศิวกร</td>
-      <td><img scr="" width="45px"</td>
+      <td><img scr="https://s359.kapook.com/pagebuilder/ab0cd56b-03fa-4cbc-bdb6-f7e79762f4c9.jpg" width="45px"</td>
       <td><a href="https://www.instagram.com/porsche.sivk/?hl=en">Instagram</a></td>  
     </tr>
     <tr>
         <td>4</td>
         <td>แจ๊คกี้ จักริน</td>
-        <td><img scr="" width="45px"></td>
+        <td><img scr="https://cms.dmpcdn.com/musicarticle/2018/03/16/14042dd5-e1ea-4e01-a0cd-bf13baad69a2.JPG" width="45px"></td>
         <td><a href="https://www.instagram.com/j.jackr/?hl=en">Instagram</a></td>
     </tr> 
     <tr>
         <td>5</td>
         <td>เติร์ด ลภัส</td>
-        <td><img scr="" width="45px"></td>
+        <td><img scr="https://i.pinimg.com/originals/5a/28/55/5a28559f35b8309fce855e3a219cb873.png" width="45px"></td>
         <td><a href="https://www.instagram.com/thirdd/">Instagram</a></td>
     </tr>
    </table>
